14f179d47ebfefc78884cb8171615b081600ed51
shengzaisui <szs121@163.com>
2018-06-20 00:32:50 +0800

    add utility classes for operation date and calendar convienient

---
+++ OutlookAgenda/Utils/CalendarCalculator.swift
@@ -0,0 +1,63 @@
+import Foundation
+
+open class CalendarCalculator {
+    
+//    static var calendar = Calendar(identifier: .gregorian)
+//
+//    static func set(local: Locale) {
+//        calendar.locale = local
+//    }
+//
+//    static func set(timezone: TimeZone) {
+//        calendar.timeZone = timezone
+//    }
+
+    static fileprivate var calendar = Calendar.autoupdatingCurrent
+    static fileprivate var currentYear = CalendarCalculator.year(of: Date())
+    
+    static func shortStandaloneMonthSymbols() -> [String] {
+        return calendar.shortStandaloneMonthSymbols
+    }
+    
+    static func day(of date: Date) -> Int {
+        return calendar.component(.day, from: date)
+    }
+    
+    static func year(of date: Date) -> Int {
+        return calendar.component(.year, from: date)
+    }
+    
+    static func month(of date: Date) -> Int {
+        return calendar.component(.month, from: date)
+    }
+    
+    static func isDateInToday(_ date: Date) -> Bool {
+        return calendar.isDateInToday(date)
+    }
+}
+
+extension Date {
+    public func day() -> Int {
+        return CalendarCalculator.day(of: self)
+    }
+    
+    public func year() -> Int {
+        return CalendarCalculator.year(of: self)
+    }
+    
+    public func month() -> Int {
+        return CalendarCalculator.month(of: self)
+    }
+    
+    public func isInCurrentYear() -> Bool {
+        return year() == CalendarCalculator.currentYear
+    }
+    
+    public func isInToday() -> Bool {
+        return CalendarCalculator.isDateInToday(self)
+    }
+    
+    public func shortStandaloneMonthSymbol() -> String {
+        return CalendarCalculator.calendar.shortStandaloneMonthSymbols[ month() - 1 ]
+    }
+}
---
+++ OutlookAgenda/Utils/DatePresenter.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+open class DatePresenter {
+    static fileprivate let dateFormatter: DateFormatter = {
+        var dateFormatter = DateFormatter()
+        return dateFormatter
+    }()
+    
+    static func string(from date: Date, dateFormat: String? = nil) -> String {
+        if dateFormat != nil {
+            dateFormatter.dateFormat = dateFormat
+        }
+        return dateFormatter.string(from: date)
+    }
+}
+
+extension Date {
+    public func string(dateFormat: String) -> String {
+        return DatePresenter.string(from: self)
+    }
+    
+
+}
